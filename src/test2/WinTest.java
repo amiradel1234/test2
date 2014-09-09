a21d1467e03994bd5d03364dd15de4ce09778500
amiradel1234 <amiradel1234@yahoo.com>
2014-09-09 20:32:14 +0300

    2nd

--- src/test2/WinTest.java
+++ src/test2/WinTest.java
@@ -1,5 +1,8 @@
 package test2;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -7,6 +10,7 @@ public class WinTest extends JFrame {
 
 	public WinTest() {
 		add(new JButton("AAA"));
+		add(new JButton("BBB"), BorderLayout.SOUTH);
 		pack();
 		setLocationRelativeTo(null);
 	}
